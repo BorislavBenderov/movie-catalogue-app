5c15b72638018b33de4953ea8a6d7253069a82b8
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-11-15 15:05:37 +0200

    Add auth context

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,29 @@
+import { createContext, useEffect, useState } from "react";
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebaseConfig';
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+    const [loggedUser, setLoggedUser] = useState(null);
+
+    useEffect(() => {
+        const unsub = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setLoggedUser(user);
+            } else {
+                setLoggedUser(null);
+            }
+        })
+        return () => {
+            unsub();
+        }
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ auth, loggedUser }}>
+            {children}
+        </AuthContext.Provider>
+    );
+
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { AuthContextProvider } from './contexts/AuthContext';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <AuthContextProvider>
+        <App />
+    </AuthContextProvider>
 );
