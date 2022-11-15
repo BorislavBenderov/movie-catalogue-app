3849dc26290acace725c2744719d8133c97cfa4d
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-11-15 16:36:16 +0200

    Add user to create movie

--- src/components/create-edit/CreateMovie.js
+++ src/components/create-edit/CreateMovie.js
@@ -1,6 +1,56 @@
+import { useContext } from "react";
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from "../../contexts/AuthContext";
+import { serverTimestamp, addDoc, collection } from 'firebase/firestore';
+import { database } from "../../firebaseConfig";
+
 export const CreateMovie = () => {
+    const { loggedUser } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const onCreate = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+
+        const title = formData.get('title');
+        const description = formData.get('description');
+        const imageUrl = formData.get('imageUrl');
+        const genre = formData.get('genre');
+        const year = formData.get('year');
+        const rating = formData.get('rating');
+
+        if (title === '' || description === '' || imageUrl === '' || genre === '' || year === '' || rating === '') {
+            alert('Please fill all the fields');
+            return;
+        }
+
+        const movieData = {
+            title,
+            description,
+            imageUrl,
+            genre,
+            year: Number(year),
+            rating: Number(rating),
+            timestamp: serverTimestamp(),
+            ownerId: loggedUser.uid,
+            likes: [],
+            comments: []
+        };
+
+        addDoc(collection(database, 'movies'), movieData)
+        .then(() => {
+            navigate('/');
+        })
+        .catch((err) => {
+            alert(err.message);
+        })
+
+    }
+
+
     return (
-        <form className="auth">
+        <form className="auth" onSubmit={onCreate}>
             <h3>Add Movie</h3>
             <label htmlFor="title"></label>
             <input type="text" placeholder="Title" id="title" name="title" />
